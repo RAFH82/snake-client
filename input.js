040a4a76e5e91cad38161eb264dc09cbc2ba7951
RAFH82 <hrechkar@gmail.com>
2020-11-04 23:42:21 +0000

    added wsad movement commands and connection object

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+// stores the tcp connection object
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,9 +11,23 @@ const setupInput = function () {
     if (key === "\u0003") {
       process.exit();
     }
+    switch (key) {
+      case "w":
+        key = conn.write("Move: up");
+        break;
+      case "a":
+        key = conn.write("Move: left");
+        break;
+      case "s":
+        key = conn.write("Move: down");
+        break;
+      case "d":
+        key = conn.write("Move: right");
+        break;
+    }
   };
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-modules.export = { setupInput };
+module.exports = { setupInput };
